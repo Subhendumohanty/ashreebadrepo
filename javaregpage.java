49444cf646e22080dae97a48b8bf0d81e38a8468
Your Name <you@example.com>
2022-02-17 17:19:20 +0530

    my 100% work complite

--- javaregpage.java
+++ javaregpage.java
@@ -1,4 +1,9 @@
 This is javapage in the index of the file 
-This is done stash file.
+This is done stash file Updated upstream
 this is my manager work has complited.
+fast day of java line of code
+2nd day of java  line of code
+3ed day of java line of code
+4th day of java line of code 90% of task has complite
+5th day of java line code 100% commit
 
